32b537f411a88504b8aec2d324d4123a77fd24ff
VladZimin <vlad.zimin1@gmail.com>
2023-01-18 09:06:46 +0500

    HW4 (refactoring)

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,11 +18,13 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        + (xType === 'red'
-            ? ' ' + s.red
-            : xType === 'secondary'
-                ? ' ' + s.secondary
-                : ' ' + s.default)
+        + (disabled
+            ? ' ' + s.disabled
+            : xType === 'red'
+                ? ' ' + s.red
+                : xType === 'secondary'
+                    ? ' ' + s.secondary
+                    : ' ' + s.default)
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
