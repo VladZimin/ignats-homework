ec2dc5145c0d329b6ac96f3424d899e2f1a5cdb6
VladZimin <vlad.zimin1@gmail.com>
2023-01-20 19:51:17 +0500

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const copyState = [...state]
+            return copyState.sort((a, b) => {
+                if (action.payload === 'up') return a.name.localeCompare(b.name)
+                return b.name.localeCompare(a.name)
+            })
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(obj => obj.age >= action.payload)
         }
         default:
             return state
