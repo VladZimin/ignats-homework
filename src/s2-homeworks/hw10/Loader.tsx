215ab742ae077e6ab20250d4514bcb3fbd6f415b
VladZimin <vlad.zimin1@gmail.com>
2023-01-21 14:23:52 +0500

    HW10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,3 @@
-import s from './Loader.module.css'
+import loader from './loader.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <img src={loader} alt="Loader"/>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,15 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+type StateType = {
+    isLoading: boolean
+}
 
+export const loadingReducer = (state = initState, action: LoadingActionType): StateType => {
+    switch (action.type) {
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
