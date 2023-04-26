99f18475dad2161dc28f796abd0a405f087e4d56
VladZimin <vlad.zimin1@gmail.com>
2023-04-26 12:06:01 +0600

    fix

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,12 +1,16 @@
-const initState = {
-    themeId: 1,
+type ActionType = {
+    type: 'SET_THEME_ID',
+    id: number
 }
-
 type StateType = {
     themeId: number
 }
 
-export const themeReducer = (state = initState, action: ActionType): StateType => { // fix any
+const initState = {
+    themeId: 1,
+}
+
+export const themeReducer = (state: StateType = initState, action: ActionType): StateType => { // fix any
     switch (action.type) {
         // дописать
         case 'SET_THEME_ID':
@@ -14,10 +18,5 @@ export const themeReducer = (state = initState, action: ActionType): StateType =
         default:
             return state
     }
-}
-
-type ActionType = {
-    type: 'SET_THEME_ID',
-    id: number
 }
 export const changeThemeId = (id: number): ActionType => ({type: 'SET_THEME_ID', id}) // fix any
